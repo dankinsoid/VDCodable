bbdce50b1763df6ee8491c34bb8b9fa76043087e
Voidilov <voidilov@gmail.com>
2020-08-08 14:10:53 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -17,14 +17,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/dankinsoid/UnwrapOperator.git", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "VDCodable",
-            dependencies: ["UnwrapOperator"]),
+            dependencies: []),
         .testTarget(
             name: "VDCodableTests",
             dependencies: ["VDCodable"]),
